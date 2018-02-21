bd7698ebdca2bf13e0699781f6990ec522e01e45
Narut Poovorakit <net.narut.p@gmail.com>
2018-02-21 18:11:19 +0700

    Add navbar

--- components/MainScreen.js
+++ components/MainScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import IconFontAwesome from 'react-native-vector-icons/FontAwesome';
+import IconEntypo from 'react-native-vector-icons/Entypo'
 import TabNavigator from 'react-native-tab-navigator';
 import NewfeedTab from './AppTabNavigator/NewfeedTab'
 import ProfileTab from './AppTabNavigator/ProfileTab'
@@ -12,38 +13,43 @@ class MainScreen extends Component {
         selectedTab: 'home'
     };
 
+    static navigationOptions = {
+        headerLeft: <IconEntypo name="menu" size={25} style={{ marginLeft:10 }} />,
+        title: "Cooklab",
+        headerRight: <IconFontAwesome name="camera" size={20} style={{ marginRight:10 }} />
+    }
     render() {
         return (
             <TabNavigator style={styles.container}>
                 <TabNavigator.Item
                     selected={this.state.selectedTab === 'home'}
                     title="Home"
-                    renderIcon={() => <Icon name="star" />}
-                    renderSelectedIcon={() => <Icon name="star" color="#3496f0" />}
+                    renderIcon={() => <IconFontAwesome name="star" size={15} />}
+                    renderSelectedIcon={() => <IconFontAwesome name="star" size={15} color="#3496f0" />}
                     onPress={() => this.setState({ selectedTab: 'home' })}>
                     <TopfeedTab />
                 </TabNavigator.Item>
                 <TabNavigator.Item 
                     selected={this.state.selectedTab === 'newfeed'}
                     title="NewFeed"
-                    renderIcon={() => <Icon name="newspaper-o" />}
-                    renderSelectedIcon={() => <Icon name="newspaper-o" color="#3496f0" />}
+                    renderIcon={() => <IconEntypo name="newsletter" size={15} />}
+                    renderSelectedIcon={() => <IconEntypo name="newsletter" size={15} color="#3496f0" />}
                     onPress={() => this.setState({ selectedTab: 'newfeed' })}>
                     <NewfeedTab />
                 </TabNavigator.Item>
                 <TabNavigator.Item
                     selected={this.state.selectedTab === 'search'}
                     title="Search"
-                    renderIcon={() => <Icon name="search" />}
-                    renderSelectedIcon={() => <Icon name="search" color="#3496f0" />}
+                    renderIcon={() => <IconFontAwesome name="search" size={15} />}
+                    renderSelectedIcon={() => <IconFontAwesome name="search" size={15} color="#3496f0" />}
                     onPress={() => this.setState({ selectedTab: 'search' })}>
                     <SearchTab />
                 </TabNavigator.Item>
                 <TabNavigator.Item
                     selected={this.state.selectedTab === 'profile'}
                     title="Profile"
-                    renderIcon={() => <Icon name="user" color="#666"/>}
-                    renderSelectedIcon={() => <Icon name="user" color="#3496f0" />}
+                    renderIcon={() => <IconFontAwesome name="user" size={15} color="#666"/>}
+                    renderSelectedIcon={() => <IconFontAwesome name="user" size={15} color="#3496f0" />}
                     onPress={() => this.setState({ selectedTab: 'profile' })}>
                     <ProfileTab />
                 </TabNavigator.Item>
