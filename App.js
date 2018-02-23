ee9eb50bbddf5af499901cca00774c7777835cbe
Natanon Poonawagul <tanlnw@gmail.com>
2018-02-23 18:24:02 +0700

    Top Feed Mockup

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation'
-import MainScreen from './components/MainScreen'
+import MainScreen from 'src/components/MainScreen'
 
 export default class App extends React.Component {
   render() {
     return (
-      <AppStackNavigator /> 
+      <AppStackNavigator />
     );
   }
 }
--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import IconFontAwesome from 'react-native-vector-icons/FontAwesome';
+<<<<<<< HEAD:components/MainScreen.js
 import IconEntypo from 'react-native-vector-icons/Entypo'
+=======
+>>>>>>> Top Feed Mockup:src/components/MainScreen.js
 import TabNavigator from 'react-native-tab-navigator';
 import NewfeedTab from './AppTabNavigator/NewfeedTab'
 import ProfileTab from './AppTabNavigator/ProfileTab'
@@ -25,32 +28,52 @@ class MainScreen extends Component {
                 <TabNavigator.Item
                     selected={this.state.selectedTab === 'home'}
                     title="Home"
+<<<<<<< HEAD:components/MainScreen.js
                     renderIcon={() => <IconFontAwesome name="star" size={15} />}
                     renderSelectedIcon={() => <IconFontAwesome name="star" size={15} color="#3496f0" />}
+=======
+                    renderIcon={() => <IconFontAwesome name="star" />}
+                    renderSelectedIcon={() => <IconFontAwesome name="star" color="#3496f0" />}
+>>>>>>> Top Feed Mockup:src/components/MainScreen.js
                     onPress={() => this.setState({ selectedTab: 'home' })}>
                     <TopfeedTab />
                 </TabNavigator.Item>
-                <TabNavigator.Item 
+                <TabNavigator.Item
                     selected={this.state.selectedTab === 'newfeed'}
                     title="NewFeed"
+<<<<<<< HEAD:components/MainScreen.js
                     renderIcon={() => <IconEntypo name="newsletter" size={15} />}
                     renderSelectedIcon={() => <IconEntypo name="newsletter" size={15} color="#3496f0" />}
+=======
+                    renderIcon={() => <IconFontAwesome name="newspaper-o" />}
+                    renderSelectedIcon={() => <IconFontAwesome name="newspaper-o" color="#3496f0" />}
+>>>>>>> Top Feed Mockup:src/components/MainScreen.js
                     onPress={() => this.setState({ selectedTab: 'newfeed' })}>
                     <NewfeedTab />
                 </TabNavigator.Item>
                 <TabNavigator.Item
                     selected={this.state.selectedTab === 'search'}
                     title="Search"
+<<<<<<< HEAD:components/MainScreen.js
                     renderIcon={() => <IconFontAwesome name="search" size={15} />}
                     renderSelectedIcon={() => <IconFontAwesome name="search" size={15} color="#3496f0" />}
+=======
+                    renderIcon={() => <IconFontAwesome name="search" />}
+                    renderSelectedIcon={() => <IconFontAwesome name="search" color="#3496f0" />}
+>>>>>>> Top Feed Mockup:src/components/MainScreen.js
                     onPress={() => this.setState({ selectedTab: 'search' })}>
                     <SearchTab />
                 </TabNavigator.Item>
                 <TabNavigator.Item
                     selected={this.state.selectedTab === 'profile'}
                     title="Profile"
+<<<<<<< HEAD:components/MainScreen.js
                     renderIcon={() => <IconFontAwesome name="user" size={15} color="#666"/>}
                     renderSelectedIcon={() => <IconFontAwesome name="user" size={15} color="#3496f0" />}
+=======
+                    renderIcon={() => <IconFontAwesome name="user" color="#666"/>}
+                    renderSelectedIcon={() => <IconFontAwesome name="user" color="#3496f0" />}
+>>>>>>> Top Feed Mockup:src/components/MainScreen.js
                     onPress={() => this.setState({ selectedTab: 'profile' })}>
                     <ProfileTab />
                 </TabNavigator.Item>
