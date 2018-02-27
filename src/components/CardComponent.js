7ee5834e366ea886b6010e858145fefe0d3484f4
Narut Poovorakit <net.narut.p@gmail.com>
2018-02-27 14:09:37 +0700

    Create card component

---
+++ src/components/CardComponent.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class NewfeedTab extends Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text> Newfeed </Text>
+            </View>
+        );
+    }
+}
+
+export default NewfeedTab;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
