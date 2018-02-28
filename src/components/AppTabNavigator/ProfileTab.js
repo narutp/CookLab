47dad5d341218a1cd38e89556f3c0e2e4e194dac
Narut Poovorakit <net.narut.p@gmail.com>
2018-02-28 20:23:24 +0700

    Add follow labels

--- src/components/AppTabNavigator/ProfileTab.js
+++ src/components/AppTabNavigator/ProfileTab.js
@@ -21,10 +21,32 @@ class ProfileTab extends Component {
                         <IconFontAwesome name="camera" size={20} style={{ marginRight:10, color: '#fff' }} />
                     </Right>
                 </Header>
+                
                 <View>
+                    {/* Cover image */}
                     <Image source={require('../../assets/image/CoverImage/coverImage1.jpg')} style={styles.coverImage} />
+                    {/* Profile image */}
                     <View style={{ alignItems: 'center' }}>
                         <Image source={require('../../assets/image/Profile/profilePic1.jpg')} style={styles.profileImage} />
+                    </View>
+                </View>
+                <View style={ styles.body }>
+                    {/* User's name */}
+                    <View style={{ alignItems: 'center' }}>
+                        <Text>NarutNrp</Text>
+                    </View>
+                    {/* Horizontal rule */}
+                    <View style={{ borderBottomColor: 'gray', borderBottomWidth: 0.5, marginTop: 5 }}></View>
+                    {/* Following | Fans */}
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                        <View style={{ alignItems: 'center' }}>
+                            <Text style={{ fontSize: 12 }}>Following</Text>
+                            <Text style={{ color: 'gray', fontSize: 11 }}>53</Text>
+                        </View>
+                        <View style={{ alignItems: 'center' }}>
+                            <Text style={{ fontSize: 12 }}>Followers</Text>
+                            <Text style={{ color: 'gray', fontSize: 11 }}>231</Text>
+                        </View>
                     </View>
                 </View>
             </View>
@@ -44,18 +66,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#4F4F4F'
   },
   coverImage: {
-    position: 'absolute',
-    resizeMode: 'stretch',
-    height: 200,
+    // position: 'absolute',
+    // resizeMode: 'stretch',
+    height: 170,
     width: '100%'
   },
   profileImage: {
     position: 'absolute',
-    top: 160,
+    top: -40,
     width: 80,
     height: 80,
     borderRadius: 37.5,
     borderWidth: 0.5,
     borderColor: 'grey'
+  },
+  body: {
+      top: 50,
   }
 })
