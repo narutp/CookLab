2ebf6ba94f27815054fef3191241619da08e50ee
Narut Poovorakit <net.narut.p@gmail.com>
2018-02-28 16:57:49 +0700

    Add cover photo in profile tab

--- src/components/AppTabNavigator/ProfileTab.js
+++ src/components/AppTabNavigator/ProfileTab.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 import IconFontAwesome from 'react-native-vector-icons/FontAwesome';
 import IconIonicons from 'react-native-vector-icons/Ionicons';
 import IconEntypo from 'react-native-vector-icons/Entypo';
-import { Container, Content, Left, Right, Body, Header } from 'native-base';
+import { Container, Content, Left, Right, Body, Header, Card, CardItem } from 'native-base';
 
 class ProfileTab extends Component {
 
@@ -21,6 +21,7 @@ class ProfileTab extends Component {
                         <IconFontAwesome name="camera" size={20} style={{ marginRight:10, color: '#fff' }} />
                     </Right>
                 </Header>
+                <Image source={require('../../assets/image/CoverImage/coverImage1.jpeg')} style={styles.coverImage} />
             </View>
         );
     }
@@ -32,8 +33,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    width: Dimensions.get('window').width
   },
   header: {
-      backgroundColor: '#4F4F4F'
+    backgroundColor: '#4F4F4F'
+  },
+  coverImage: {
+    resizeMode: 'stretch',
+    height: 200,
+    width: '100%'
   }
 })
