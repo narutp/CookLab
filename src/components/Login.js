60abe7a906df3ae0e7d73cf8132b1609f3369a16
Narut Poovorakit <net.narut.p@gmail.com>
2018-03-12 22:32:26 +0700

    Add background image

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 class Login extends Component {
     
     render() {
         return (
             <View>
-                <Text> Login </Text>
+                <Text style={ styles.title }> CookLab </Text>
             </View>
         )
     }
 }
 
 export default Login
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 30,
+        textAlign: 'center'
+    }
+})
