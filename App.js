fccb4537dbbdcbc842ac0a8e6ced29cd6f2e84ef
Narut Poovorakit <net.narut.p@gmail.com>
2018-03-28 13:35:30 +0700

    Hide the status bar on Android

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { StackNavigator } from 'react-navigation'
-import { DrawerNavigator } from 'react-navigation'
+import { StyleSheet, Text, View, StatusBar  } from 'react-native'
+import { StackNavigator, DrawerNavigator } from 'react-navigation'
 import MainScreen from 'src/components/MainScreen'
 import MyDishes from 'src/components/AppDrawerNavigator/MyDishes'
 import SideMenu from 'src/components/SideMenu'
 import Router from './src/components/DrawerRouter'
 import Login from './src/components/Login'
-import SplashScreen from 'react-native-splash-screen';
+import SplashScreen from 'react-native-splash-screen'
 
 export default class App extends React.Component {
   componentDidMount() {
     SplashScreen.hide()
+    StatusBar.setHidden(true)
   }
 
   render() {
