87c3ae35e49ea534d0b4b32d88f40a5088f8920d
Narut Poovorakit <net.narut.p@gmail.com>
2018-04-08 20:29:32 +0700

    Fix some detail after merge

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,16 +7,17 @@ import Register from 'src/components/Register'
 import { Provider } from 'react-redux'
 import React from 'react'
 import store from 'src/redux/store'
+import DrawerRouter from '../components/DrawerRouter';
 
 const App = () => {
     return (
         <Provider store={store}>
             <Router>
                 <Scene key="root">
-                    <Scene key="Login" component={Login} initial />
-                    <Scene key="MainScreen" component={MainScreen} />
-                    <Scene key="StatusPosting" component={StatusPosting} />
-                    <Scene key="Register" component={Register} />
+                    <Scene key="Login" component={Login} hideNavBar={1} initial />
+                    <Scene key="MainScreen" component={MainScreen} hideNavBar={1} />
+                    <Scene key="StatusPosting" component={StatusPosting} hideNavBar={1} />
+                    <Scene key="Register" component={Register} hideNavBar={1} />
                 </Scene>
             </Router>
         </Provider>
