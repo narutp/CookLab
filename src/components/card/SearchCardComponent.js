f4fb05a00fbedf6b8ee9ed42743817187d707e92
Narut Poovorakit <net.narut.p@gmail.com>
2018-04-15 16:20:45 +0700

    Modify search card component

--- src/components/card/SearchCardComponent.js
+++ src/components/card/SearchCardComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text } from 'react-native'
-import { Card, CardItem } from 'native-base'
+import { StyleSheet, Text, View } from 'react-native'
+import { Card, CardItem, Left, Body, Right } from 'native-base'
 
 class SearchCardComponent extends Component {
 
@@ -8,7 +8,18 @@ class SearchCardComponent extends Component {
         return (
             <Card>
                 <CardItem>
-                    <Text>Hello</Text>
+                    <Left>
+                        {/* <Thumbnail source={profileImage[this.props.profilePic]} style={{ width: 30, height: 30 }}/> */}
+                        <Body>
+                            <Text>Big spinach hamburger</Text>
+                            <Text note style={{ fontSize: 9 }}>Dish level 2</Text>
+                        </Body>
+                    </Left>
+                    <Right>
+                        <Body style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <Text>3/5</Text>
+                        </Body>
+                    </Right>
                 </CardItem>
             </Card>
         )
