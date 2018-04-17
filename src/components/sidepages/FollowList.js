9753da840a3ab8f40a2e95517d9c5c943c9cad54
Narut Poovorakit <net.narut.p@gmail.com>
2018-04-18 02:59:16 +0700

    Finish follow list page

--- src/components/sidepages/FollowList.js
+++ src/components/sidepages/FollowList.js
@@ -1,15 +1,46 @@
 import React, {Component} from 'react'
-import { StyleSheet, View, ScrollView } from 'react-native'
+import { StyleSheet, View, ScrollView, AsyncStorage } from 'react-native'
 import { Container, Header, Content, List, ListItem, Left, Body, Button, Right, Thumbnail, Text } from 'native-base'
 import BackHeader from '../header/BackHeader'
+import CooklabAxios from '../../http/index'
 
 class FollowList extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
+            isFollow: true
         }
     }
+
+    async followUser(targetId) {
+        let getUserId
+        try {
+            getUserId = await AsyncStorage.getItem('userid')
+        } catch (error) {
+            console.log(error)
+        }
+        let followResponse
+        try {
+            followResponse = await CooklabAxios.put(`follow`, {
+                userId: getUserId,
+                targetId: targetId
+            })
+        } catch (error) {
+            console.log(error)
+        }
+        console.log('Follow response: ', followResponse.data)
+        if (followResponse.data === 'follow') {
+            this.setState({
+                isFollow: true
+            })
+        } else {
+            this.setState({
+                isFollow: false
+            })
+        }
+    }
+
     render() {
         console.log('Follow list page: ', this.props.data)
         return (
@@ -21,18 +52,20 @@ class FollowList extends Component {
                             <List>
                                 <ListItem avatar>
                                     <Left>
-                                        <Thumbnail style={{ width: 40, height: 40 }} source={{ uri: element.photo }} />
+                                        <Thumbnail style={{ width: 45, height: 40 }} source={{ uri: element.photo }} />
                                     </Left>
                                     <Body>
                                         <Text style={{ fontSize: 12 }}>{element.name}</Text>
                                     </Body>
                                     <Right>
-                                        <Button light style={ styles.button }>
-                                            <Text style={{ color: 'black', fontSize: 11 }}>Following</Text>
-                                        </Button>
-                                        <Button primary style={ styles.button }>
-                                            <Text style={{ color: 'white', fontSize: 11 }}>Follow</Text>
-                                        </Button>
+                                        { this.state.isFollow === true ? 
+                                            <Button onPress={ () => this.followUser(element._id) } light style={ styles.button }>
+                                                <Text style={{ color: 'black', fontSize: 9 }}>Following</Text>
+                                            </Button> :
+                                            <Button onPress={ () => this.followUser(element._id) } primary style={ styles.button }>
+                                                <Text style={{ color: 'white', fontSize: 9 }}>Follow</Text>
+                                            </Button>
+                                        }
                                     </Right>
                                 </ListItem>
                             </List>
@@ -51,11 +84,14 @@ const styles = StyleSheet.create({
         flex: 1     
     },
     listWrapper: {
-        marginTop: 10
+        marginTop: 10,
+        marginBottom: 5
     },
     button: {
-        padding: 5,
-        width: 80,
-        height: 30
+        padding: 2,
+        width: 90,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
