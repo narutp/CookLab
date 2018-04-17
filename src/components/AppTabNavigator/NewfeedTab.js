048343ceee418d39bf17807880707a6bfb81a08a
Narut Poovorakit <net.narut.p@gmail.com>
2018-04-17 21:39:31 +0700

    Add refresh features

--- src/components/AppTabNavigator/NewfeedTab.js
+++ src/components/AppTabNavigator/NewfeedTab.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
-import IconFontAwesome from 'react-native-vector-icons/FontAwesome';
-import IconIonicons from 'react-native-vector-icons/Ionicons';
-import IconEntypo from 'react-native-vector-icons/Entypo';
+import React, { Component } from 'react'
+import { ScrollView, StyleSheet, Text, View, AsyncStorage, RefreshControl } from 'react-native'
+import IconFontAwesome from 'react-native-vector-icons/FontAwesome'
+import IconIonicons from 'react-native-vector-icons/Ionicons'
+import IconEntypo from 'react-native-vector-icons/Entypo'
 import AppHeader from '../header/AppHeader'
-import { Container, Content, Left, Right, Body } from 'native-base';
+import { Container, Content, Left, Right, Body } from 'native-base'
 import CardComponent from '../CardComponent.js'
 import CooklabAxios from '../../http/index'
 import moment from 'moment'
@@ -40,37 +40,51 @@ class NewfeedTab extends Component {
         super(props)
         this.state = {
             feedResponse: [],
-            user: ''
+            user: '',
+            refreshing: false
         }
     }
 
+    _onRefresh() {
+        this.setState({
+            refreshing: true
+        })
+
+        this.fetchPost().then(() => {
+            this.setState({refreshing: false})
+        })
+    }
+    
     render() {
         return (
             <Container style={styles.container}>
                 <AppHeader onMenuPressed={ this.props.onMenuPressed } showCameraRoll={ this.props.showCameraRoll } />
-                <Content>
-                    {this.state.feedResponse.map((data, index) => {
-                        return (
-                            <CardComponent 
-                                trophy={data.trophies} 
-                                profilePic={data.photo} 
-                                foodPic={data.image} 
-                                caption={data.caption}
-                                date={moment.utc(data.timestamp).format("MMMM Do YYYY, h:mm a")}
-                                userName={data.user_name}
-                                postId={data._id}
-                                status={data.status}
-                                comments={data.comments}
-                                idDish={data.id_dish}
-                             />
-                        )
-                    })}
-                    {/* <CardComponent love='176' profilePic='1' foodPic='1' />
-                    <CardComponent love='71' profilePic='2' foodPic='2' />
-                    <CardComponent love='26' profilePic='1' foodPic='3' />
-                    <CardComponent love='34' profilePic='2' foodPic='4' />
-                    <CardComponent love='102' profilePic='2' foodPic='5' /> */}
-                </Content>
+                    <View>
+                        <ScrollView
+                            refreshControl={
+                                <RefreshControl
+                                    refreshing={this.state.refreshing}
+                                    onRefresh={this._onRefresh.bind(this)}
+                                />
+                        }>
+                            {this.state.feedResponse.map((data, index) => {
+                                return (
+                                    <CardComponent 
+                                        trophy={data.trophies} 
+                                        profilePic={data.photo} 
+                                        foodPic={data.image} 
+                                        caption={data.caption}
+                                        date={moment.utc(data.timestamp).format("MMMM Do YYYY, h:mm a")}
+                                        userName={data.user_name}
+                                        postId={data._id}
+                                        status={data.status}
+                                        comments={data.comments}
+                                        idDish={data.id_dish}
+                                    />
+                                )
+                            })}
+                        </ScrollView>
+                    </View>
             </Container>
         )
     }
