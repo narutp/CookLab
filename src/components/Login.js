0ef028e5dac7cf43a3bac10ea7a77d4ea9c71b0c
Narut Poovorakit <net.narut.p@gmail.com>
2018-05-01 20:57:48 +0700

    Share facebook

--- src/components/Login.js
+++ src/components/Login.js
@@ -253,6 +253,21 @@ class Login extends Component {
                             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                                 <Text style={ styles.textButton }>Login by Facebook</Text>
                             </View>
+                            {/* <LoginButton
+                                publishPermissions={["publish_actions"]}
+                                onLoginFinished={
+                                    (error, result) => {
+                                    if (error) {
+                                        alert("Login failed with error: " + error.message);
+                                    } else if (result.isCancelled) {
+                                        alert("Login was cancelled");
+                                    } else {
+                                        this._fbAuth()
+                                        // alert("Login was successful with permissions: " + result.grantedPermissions)
+                                    }
+                                    }
+                                }
+                                onLogoutFinished={() => alert("User logged out")}/> */}
                         </Button>
                     </View>
                 </View>
